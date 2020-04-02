5cea607a20713f2891eb48156fc177a365b53e8f
franciscaluz <vfranciscaluz@gmail.com>
2020-04-02 08:52:26 -0400

    Final Project add background

--- Programmation1/final/assets/js/exercice_1.js
+++ Programmation1/final/assets/js/exercice_1.js
@@ -4,26 +4,36 @@ $('#first_form').submit(function(e) {
     var alphabet = /^[a-zA-Z]+$/;
 
     $("#form1-error").hide();
+    $('.result-col').hide();
 
     if (myInput.length < 3) {
         $("#form1-error").show().append('Votre mot doit contenir un minimum de 3 caractères!');
         return false;
     }
-
     if(myInput.match(alphabet)) {
-        $('.letter-wrapper').hide();
-        for(let i=0; i < myInput.length; i++) {
-            // let path = "images/letters/" + myInput[i] + "/" + myInput[i] + "1";
-            $('.letter-wrapper').show();
-            $('.result-image').attr('src', "images/letters/" + myInput[i] + "/" + myInput[i] + "1");
-        }
+        for (let i = 0; i < myInput.length; i++) {
+            path = "images/letters/" + myInput[i] + "/" + myInput[i] + "1.jpg";
 
+            $('.result-col').show().append('<a href="#exampleModal" data-toggle="modal" data-target="#exampleModal" class="result-link"><img src="" alt="" class="img-fluid result-image"></a>');
+            $('.result-image').attr('src', path);
+
+            if(myInput[i] === "é") {
+                $('.result-image').attr('src','assets/images/E/E1.jpg');
+            }
+
+        }
     } else {
         $("#form1-error").show().append('Votre mot ne doit pas contenir de chiffres!');
     }
-
 });
 
+
+
+
+// path_pattern = 'images/letters/<letter>/<letter>1';
+// for(var i=0; i < myInput.length; i++) {
+//     path = path_pattern.replace('<letter>',  myInput[i]);
+// }
 
 
 
