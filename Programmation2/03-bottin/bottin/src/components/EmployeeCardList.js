4a60f38c22e851bd67db390a78f6de6ebfe7d6e3
franciscaluz <vfranciscaluz@gmail.com>
2020-05-20 11:13:36 -0400

    Bottin update

--- Programmation2/03-bottin/bottin/src/components/EmployeeCardList.js
+++ Programmation2/03-bottin/bottin/src/components/EmployeeCardList.js
@@ -1,38 +1,44 @@
-import React, { PureComponent } from 'react';
-import styled from 'styled-components/macro'
-import EmployeeCardSingle from '../components/EmployeeCardSingle'
+import React, { PureComponent } from "react";
+import styled from "styled-components/macro";
+import EmployeeCardSingle from "../components/EmployeeCardSingle";
 
 //import { Container, Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 class EmployeeCardList extends PureComponent {
-
-    render() {  
-        return (
-            <Wrapper>
-               <div class="row row-cols-1 row-cols-sm-3 row-cols-lg-4 row-cols-xl-6">
-                    <EmployeeCardSingle
-                    name= "Francisca Vargas"
-                    title="Developpeur Front-End"
-                    email='francisca@random.com'
-                    telephone='(514) 666-6666'
-                    address= '1234, rue St-Denis, Montréal, Québec'
-                    />
-                    <EmployeeCardSingle/>
-                    <EmployeeCardSingle/>
-                    <EmployeeCardSingle/>
-                    <EmployeeCardSingle/>
-                    <EmployeeCardSingle/>
-                    <EmployeeCardSingle/>
-                    <EmployeeCardSingle/>
-                </div>
-            </Wrapper>
-        )
-    }
+  render() {
+    return (
+      <Wrapper>
+        <div class="row row-cols-1 row-cols-sm-3 row-cols-lg-4 row-cols-xl-5">
+          <EmployeeCardSingle
+            color= '#eeeeee'
+            name="Shany Carl"
+            title="Enseignant en Informatique"
+            email="shany@random.com"
+            telephone="418-666-6666"
+            address="1234, rue Thiers"
+          />
+          <EmployeeCardSingle
+            color = '#cccccc'
+            name="Francisca Vargas"
+            title="Developpeur Front-End"
+            email="francisca@random.com"
+            telephone="+514-555-5555"
+            address="1234, rue St-Denis"
+          />
+          <EmployeeCardSingle
+            color = '#9a9a9a'
+            name="Julien Laplanche"
+            title="Developpeur Front-End"
+            email="julien@random.com"
+            telephone="+514-444-4444"
+            address="1234, rue St-Catherine"
+          />
+        </div>
+      </Wrapper>
+    );
+  }
 }
-
 
 export default EmployeeCardList;
 
-const Wrapper = styled.div`
-
-`
+const Wrapper = styled.div``;
--- Programmation2/03-bottin/bottin/src/components/EmployeeCardSingle.js
+++ Programmation2/03-bottin/bottin/src/components/EmployeeCardSingle.js
@@ -4,23 +4,29 @@ import { Card, ListGroup } from "react-bootstrap";
 
 class EmployeeCardSingle extends PureComponent {
   render() {
-    const { name, title, email, telephone, address } = this.props;
+    const { name, title, email, telephone, address, color } = this.props;
 
     return (
       <Wrapper className="col employee-single-card-col">
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        <Card className='employee-card-single'>
+          <div className='employee-card-single-img'></div>
           <Card.Body>
             <ListGroup variant="flush">
-            <ListGroup.Item>
-                <Card.Title>{name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
-            </ListGroup.Item>
               <ListGroup.Item>
-                  <a href="mailto:{email}">{email}</a>
-                  </ListGroup.Item>
-              <ListGroup.Item> <a href="mailto:{telephone}">{telephone}</a></ListGroup.Item>
-              <ListGroup.Item><span>{address}</span></ListGroup.Item>
+                <Card.Title>{name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {title}
+                </Card.Subtitle>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <a href={`mailto:${this.props.email}`}>{email}</a>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <a href={`tel:+${this.props.telephone}`}>{telephone}</a>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <span>{address}</span>
+              </ListGroup.Item>
             </ListGroup>
           </Card.Body>
         </Card>
@@ -31,4 +37,12 @@ class EmployeeCardSingle extends PureComponent {
 
 export default EmployeeCardSingle;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+.employee-card-single-img {
+    display: block;
+    height: 150px;
+    width: 100%;
+    background-color: ${this.props.color};
+
+
+`
