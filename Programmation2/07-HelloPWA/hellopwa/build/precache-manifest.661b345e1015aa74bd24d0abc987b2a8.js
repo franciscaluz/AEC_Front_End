2b94097d0b5ce56feee17121b332ef193ac1d826
franciscaluz <vfranciscaluz@gmail.com>
2020-08-25 20:50:44 -0400

    test pwa publication 2

---
+++ Programmation2/07-HelloPWA/hellopwa/build/precache-manifest.661b345e1015aa74bd24d0abc987b2a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6915e416483760865d7fc7fe14e89ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2eff57d7a7da7c438296",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "82f370b39da0dfdd9c4e",
+    "url": "./static/css/main.d2d1b2e8.chunk.css"
+  },
+  {
+    "revision": "2eff57d7a7da7c438296",
+    "url": "./static/js/2.9a096318.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.9a096318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f370b39da0dfdd9c4e",
+    "url": "./static/js/main.ef7765ec.chunk.js"
+  },
+  {
+    "revision": "596db6d2290197e71c87",
+    "url": "./static/js/runtime-main.cf705ba3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- Programmation2/07-HelloPWA/hellopwa/build/service-worker.js
+++ Programmation2/07-HelloPWA/hellopwa/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82d7a13135854d7a03abc39abe12919b.js"
+  "./precache-manifest.661b345e1015aa74bd24d0abc987b2a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
