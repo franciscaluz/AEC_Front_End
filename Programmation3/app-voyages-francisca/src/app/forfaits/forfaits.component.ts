0c5f847109913059fa7b724a2775ef792f7a609b
franciscaluz <vfranciscaluz@gmail.com>
2020-11-25 22:42:23 -0500

    forfait-voyages // forfaits

---
+++ Programmation3/app-voyages-francisca/src/app/forfaits/forfaits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forfaits',
+  templateUrl: './forfaits.component.html',
+  styleUrls: ['./forfaits.component.scss']
+})
+export class ForfaitsComponent implements OnInit {
+
+  constructor() { }
+  showFiller = false;
+  ngOnInit(): void {
+  }
+
+}
